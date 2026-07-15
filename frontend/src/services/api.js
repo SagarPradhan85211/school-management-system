db916fcc4050a7173368a724f106c94b1ffd2841
Sagar Pradhan <sagarpradhan781@gmail.com>
2026-07-15 23:48:25 +0530

    Connect frontend to Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://school-management-system-rwa9.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
